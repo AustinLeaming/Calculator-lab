e3fafa3bd6f2be427f08a7d19d64ecf25d5a07c0
AustinLeaming <austin@Madisons-MacBook-Air.local>
2021-09-06 16:07:21 -0600

    operations complete. need to address: only one integer

--- js/main.js
+++ js/main.js
@@ -22,36 +22,82 @@ keys.addEventListener('click', e => {
         //declares the text in the output screen
         const displayNum = display.textContent;
 
+        const previousKeyType = calculator.dataset.previousKeyType;
+
         //if the button DOESNT have a data-action (numbers) do this
         if (!action) {
-            if (displayNum === '0') {
+            //if the text content within the display shows a zero, or is an operator -
+            if (displayNum === '0' || previousKeyType === 'operator') {
+                // - make the text content equal to the text content of key pressed
                 display.textContent = keyContent;
             } else {
+                // - make the display text content equal to the display content plus whatever key was pressed
                 display.textContent = displayNum + keyContent;
             }
+
         }
 
         //if the button DOES have a data action (operators) of the following:
         if (action ==='add' || action ==='minus' || action ==='multiply'
         || action === 'divide') {
             console.log('operator key!')
+            //adds a class to the button if its pressed, changes the color
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.firstValue = displayNum;
+            calculator.dataset.operator = action;
         }
+        
 
         //if the button clicked is 'clear' button
         if (action==='clear') {
             console.log('clear key')
             display.textContent = '0'
+            //removes .is-depressed from all keys
+            Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
         }
 
         //if the button clicked is 'decimal' button
         if (action === 'decimal') {
-            console.log('decimal key')
+            display.textContent = displayNum + '.';
+        }
+
+
+        const calculate = (n1, operator, n2) => {
+            let result = '';
+
+            if (operator === 'add') {
+                result = parseFloat(n1) + parseFloat(n2);
+            } else if (operator === 'minus') {
+                result = parseFloat(n1) - parseFloat(n2);
+            } else if (operator === 'multiply') {
+                result = parseFloat(n1) * parseFloat(n2);
+            } else if (operator === 'divide') {
+                result = parseFloat(n1) / parseFloat(n2);
+            }
+
+            return result;
         }
 
         //if the button clicked is 'equal' button
         if (action === 'equal') {
+
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayNum;
+
+            display.textContent = calculate(firstValue, operator, secondValue);
             console.log('equal key');
+            //removes .is-depressed from all keys
+            Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
         }
+
+        
+
+        
+        
     }
 
 
